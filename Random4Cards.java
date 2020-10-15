b6bfb186cb5406d9024a64f23d7547c30216ef73
YaninaBankova <43926387+YaninaBankova@users.noreply.github.com>
2020-10-15 11:38:25 +0300

    Add files via upload

--- Random4Cards.java
+++ Random4Cards.java
@@ -3,7 +3,6 @@ package edu.smg;
 import java.util.Random;
 
 public class Random4Cards {
-
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		String[] colorKind = {"Clubs","Diamonds","Hearts","Spades"};
@@ -18,6 +17,26 @@ public class Random4Cards {
 			cards[i][0] = rank;
 			cards[i][1] = color;
 		}
+		boolean sameCard = false;
+		do {
+			for(int i = 0; i < 4; i++) {
+				if(i < 3 && cards[i][0] == cards[i+1][0] && cards[i][1] == cards[i+1][0]) {
+					sameCard = true;
+					cards[i][0] = rand.nextInt(13);
+					cards[i][1] = rand.nextInt(4);
+				}
+				if(i < 2 && cards[i][0] == cards[i+2][0] && cards[i][1] == cards[i+2][0]) {
+					sameCard = true;
+					cards[i][0] = rand.nextInt(13);
+					cards[i][1] = rand.nextInt(4);
+				}
+				if(cards[0][0] == cards[3][0] && cards[0][1] == cards[3][0]) {
+					sameCard = true;
+					cards[i][0] = rand.nextInt(13);
+					cards[i][1] = rand.nextInt(4);
+				}
+			}
+		} while(sameCard);
 		for(int i = 0; i < 4; i++){
 			System.out.println(rankKind[cards[i][0]] + " of " + colorKind[cards[i][1]]);
 		}
