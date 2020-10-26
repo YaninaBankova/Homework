3ba99a7bb88c58b2948e791277d5a9cb5a49ef0d
YaninaBankova <43926387+YaninaBankova@users.noreply.github.com>
2020-10-26 10:14:40 +0200

    Add files via upload

--- Sudoku.java
+++ Sudoku.java
@@ -5,57 +5,46 @@ import java.util.Scanner;
 public class Sudoku {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-		Scanner sc = new Scanner(System.in);
+		int [][] sudoku = readASolution();
+		System.out.println(isValid(sudoku) ? "Valid solution!":"Invalid solution.");
+	}
+	public static int [][] readASolution(){
+		System.out.println("Enter a sudoku solution!");
 		int[][] sudoku = new int[9][9];
+		Scanner sc = new Scanner(System.in);
 		for(int i = 0; i < 9; i++) {
-			System.out.println("Enter the " + (i + 1) + " row.");
 			for(int j = 0; j < 9; j++) {
 				sudoku[i][j] = sc.nextInt();
 			}
 		}
 		sc.close();
-		boolean notSudoku = false;
+		return sudoku;
+	}
+	public static boolean isValid(int[][] sudoku) {
 		for(int i = 0; i < 9; i++) {
-			for(int k = 1; k < 10; k++) {
-				for(int j = 0; j < 9; j++) {
-					if(k == sudoku[i][j]) {
-						notSudoku = false;
-						break;
-					} else {
-						notSudoku = true;
-					}
-				}
-				if(notSudoku)
-					break;
+			for(int j = 0; j < 9; j++) {
+				if(sudoku[i][j] < 1 || sudoku[i][j] > 9 || !isValid(i, j, sudoku))
+					return false;
 			}
-			if(notSudoku)
-				break;
 		}
-		while(!notSudoku) {
-			for(int i = 0; i < 9; i++) {
-				for(int k = 1; k < 10; k++) {
-					for(int j = 0; j < 9; j++) {
-						if(k == sudoku[j][i]) {
-							notSudoku = false;
-							break;
-						} else {
-							notSudoku = true;
-						}
-					}
-					if(notSudoku)
-						break;
-				}
-				if(notSudoku)
-					break;
+		return true;
+	}
+	public static boolean isValid(int i, int j, int[][] sudoku) {
+		for(int col = 0; col < 9; col++) {
+			if(col != j && sudoku [i][j]==sudoku[i][col])
+				return false;
+		}
+		for(int row = 0; row < 9; row++) {
+			if(row != i && sudoku[i][j] == sudoku[row][j])
+			return false;
+		}
+		for(int row = (i/3)*3; row < (i/3)*3+3; row++) {
+			for(int col = (j/3)*3; col < (j/3)*3+3; col++) {
+				if((col != j || row != i) && sudoku[row][col] == sudoku[i][j])
+					return false;
 			}
-			break;
 		}
-		if(notSudoku) {
-			System.out.println("This is not a solution for sudoku!");
-		} else {
-			System.out.println("This is a solution for sudoku!");
-		}
+		return true;
 	}
 
 }
